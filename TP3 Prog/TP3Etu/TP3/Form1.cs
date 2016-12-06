ba69739654767f90a00feb8d86c30de9515e4c4f
Antony Langevin <mr.antoknee@gmail.com>
2016-12-06 08:54:14 -0500

    Comment et ma partie terminée

--- TP3 Prog/TP3Etu/TP3/Form1.cs
+++ TP3 Prog/TP3Etu/TP3/Form1.cs
@@ -22,16 +22,20 @@ namespace TP3
       timerDescente.Stop();
       FonctionsJeu();
     }
-    //NEED COMMENTS
     int nbColonnesJeu = 10;
     int nbLignesJeu = 20;
+    //Représente la position ajouté sur pour le Y ou le X
     int addY = 0;
     int addX = 0;
+    //Représente lenuméro de la pièce qui est joué
     int pieceAleatoire = 0;
     bool musiqueActive = false;
+    //Représente le tableau du jeu avec tout les états des blocs à l'intérieur
     int[,] etatBlocs = null;
+    //Représente les blocs de la pièce en jeu
     int[] blocActifY = null;
     int[] blocActifX = null;
+    //Tableau qui compte combien de pièce différente il y a eu au cours de la partie
     public float[] nbrBlocsDifferent = new float[9];
     Color[] toutesLesCouleurs = new Color[] { Color.Black, Color.Gray, Color.Red, Color.Teal, Color.Teal };
     // Variable nécessaire pour jouer le son
@@ -101,8 +105,10 @@ namespace TP3
       TestRotationHoraireGauche();
       TestRotationHoraireDroite();
       TestRotationHoraireCentreAvecBlocs();
+      TestFinDePartie();
+      TestContinuerPartie();
     }
-
+    //<alangevin>
     /// <summary>
     /// Fonction qui test la rotation horaire d'une pièce au centre du tableau
     /// </summary>
@@ -110,16 +116,16 @@ namespace TP3
     {
       // Mise en place des données du test
       Deplacement sens = Deplacement.ROTATION_HORAIRE;
-      int[] blocY = AssignerPositionFormeY(2);
-      int[] blocX = AssignerPositionFormeX(2);
+      blocActifY = AssignerPositionFormeY(2);
+      blocActifX = AssignerPositionFormeX(2);
       addX = 5;
       addY = 10;
       bool testReponse;
 
       InitialiserSurfaceDeJeu(nbLignesJeu, nbColonnesJeu);
-      for (int i = 0; i < blocY.Length - 1; i++)
+      for (int i = 0; i < blocActifY.Length; i++)
       {
-        etatBlocs[(blocY[i] + addY), blocX[i] + addX] = (int)TypeBloc.Carré;
+        etatBlocs[(blocActifY[i] + addY), blocActifX[i] + addX] = (int)TypeBloc.Carré;
       }
       // Exécuter de la méthode à tester
       testReponse = BlocPeutBouger(sens);
@@ -138,23 +144,23 @@ namespace TP3
     {
       // Mise en place des données du test
       Deplacement sens = Deplacement.ROTATION_HORAIRE;
-      int[] blocY = AssignerPositionFormeY(2);
-      int[] blocX = AssignerPositionFormeX(2);
+      blocActifY = AssignerPositionFormeY(2);
+      blocActifX = AssignerPositionFormeX(2);
       addX = 0;
       addY = 10;
       bool testReponse;
 
       InitialiserSurfaceDeJeu(nbLignesJeu, nbColonnesJeu);
-      for (int i = 0; i < blocY.Length - 1; i++)
+      for (int i = 0; i < blocActifY.Length; i++)
       {
-        etatBlocs[(blocY[i] + addY), blocX[i] + addX] = (int)TypeBloc.Carré;
+        etatBlocs[(blocActifY[i] + addY), blocActifX[i] + addX] = (int)TypeBloc.Carré;
       }
       // Exécuter de la méthode à tester
 
       testReponse = BlocPeutBouger(sens);
       // Validation des résultats
 
-        Debug.Assert(testReponse == true);
+        Debug.Assert(testReponse == false);
 
       // Clean-up
       RefairePartie();
@@ -183,26 +189,28 @@ namespace TP3
       testReponse = BlocPeutBouger(sens);
       // Validation des résultats
 
-      Debug.Assert(testReponse == false);
+      Debug.Assert(testReponse == true);
 
       // Clean-up
       RefairePartie();
     }
-
+    /// <summary>
+    /// Fonction qui test la rotation horaire d'une pièce au centre du tableau, mais avec des blocs gelés autour
+    /// </summary>
     void TestRotationHoraireCentreAvecBlocs()
     {
       // Mise en place des données du test
       Deplacement sens = Deplacement.ROTATION_HORAIRE;
-      int[] blocY = AssignerPositionFormeY(2);
-      int[] blocX = AssignerPositionFormeX(2);
+      blocActifY = AssignerPositionFormeY(2);
+      blocActifX = AssignerPositionFormeX(2);
       addX = 5;
       addY = 10;
       bool testReponse;
 
       InitialiserSurfaceDeJeu(nbLignesJeu, nbColonnesJeu);
-      for (int i = 0; i < blocY.Length - 1; i++)
+      for (int i = 0; i < blocActifY.Length; i++)
       {
-        etatBlocs[(blocY[i] + addY), blocX[i] + addX] = (int)TypeBloc.Carré;
+        etatBlocs[(blocActifY[i] + addY), blocActifX[i] + addX] = (int)TypeBloc.Carré;
       }
       etatBlocs[9, 5] = (int)TypeBloc.Gelé;
       etatBlocs[9, 6] = (int)TypeBloc.Gelé;
@@ -221,9 +229,73 @@ namespace TP3
       // Clean-up
       RefairePartie();
     }
+    /// <summary>
+    /// Fonction qui test si la fin de partie fonctionne correctement lorsqu'il y en a une
+    /// </summary>
+    void TestFinDePartie()
+    {
+
+      // Mise en place des données du test
+      blocActifY = AssignerPositionFormeY(2);
+      blocActifX = AssignerPositionFormeX(2);
+      addX = 5;
+      addY = 0;
+      bool testPartieTermine;
+
+      InitialiserSurfaceDeJeu(nbLignesJeu, nbColonnesJeu);
+      for (int i = 0; i < blocActifY.Length; i++)
+      {
+        etatBlocs[(blocActifY[i] + addY), blocActifX[i] + addX] = (int)TypeBloc.Carré;
+      }
+      etatBlocs[0, 5] = (int)TypeBloc.Gelé;
+
+      // Exécuter de la méthode à tester
+      testPartieTermine = VerifierSiPartieTermine();
+      // Validation des résultats
+
+      Debug.Assert(testPartieTermine == true);
+
+      // Clean-up
+      RefairePartie();
+    }
+
+    /// <summary>
+    /// Fonction qui test si la fin de partie fonctionne correctement lorsque le jeu doit continuer
+    /// </summary>
+    void TestContinuerPartie()
+    {
+
+      // Mise en place des données du test
+      blocActifY = AssignerPositionFormeY(2);
+      blocActifX = AssignerPositionFormeX(2);
+      addX = 0;
+      addY = 0;
+      bool testPartieTermine;
+
+      InitialiserSurfaceDeJeu(nbLignesJeu, nbColonnesJeu);
+      for (int i = 0; i < blocActifY.Length; i++)
+      {
+        etatBlocs[(blocActifY[i] + addY), blocActifX[i] + addX] = (int)TypeBloc.Carré;
+      }
+      etatBlocs[0, 5] = (int)TypeBloc.Gelé;
+
+      // Exécuter de la méthode à tester
+      testPartieTermine = VerifierSiPartieTermine();
+      // Validation des résultats
+
+      Debug.Assert(testPartieTermine == false);
+
+      // Clean-up
+      RefairePartie();
+    }
+    //</alangevin>
     #endregion
 
-
+    /// <summary>
+    /// Fonction qui vérifie si la pièce peut effectuer le mouvement attendu
+    /// </summary>
+    /// <param name="sens"></param>
+    /// <returns>Retourne un booléen qui affirme ou non si la pièce peut bouger</returns>
     bool BlocPeutBouger(Deplacement sens)
     {
       bool peutBouger = true;
@@ -276,27 +348,27 @@ namespace TP3
       //Vérification, bouger la pièce du sens anti-horaire
       else if (sens == Deplacement.ROTATION_ANTIHORAIRE)
       {
-        int[] temporaireTableauY = new int[4];
         int[] temporaireTableauX = new int[4];
+        int[] temporaireTableauY = new int[4];
         for (int compteur = 0; compteur < blocActifX.Length; compteur++)
         {
-          temporaireTableauY[compteur] = blocActifY[compteur];
-          temporaireTableauX[compteur] = -blocActifX[compteur];
+          temporaireTableauX[compteur] = blocActifY[compteur];
+          temporaireTableauY[compteur] = -blocActifX[compteur];
         }
         for (int i = 0; i < blocActifX.Length; i++)
         {
           // Vérification des côtés du tableau
-          if ((temporaireTableauY[i] + addX) >= nbColonnesJeu || (temporaireTableauY[i] + addX) < 0)
+          if ((temporaireTableauX[i] + addX) >= nbColonnesJeu || (temporaireTableauX[i] + addX) < 0)
           {
             peutBouger = false;
           }
           // Vérification haut et bas du tableau
-          else if ((temporaireTableauX[i] + addY) >= nbLignesJeu || (temporaireTableauX[i] + addY) < 0)
+          else if ((temporaireTableauY[i] + addY) >= nbLignesJeu || (temporaireTableauY[i] + addY) < 0)
           {
             peutBouger = false;
           }
           // Vérification des blocs gelés
-          else if (etatBlocs[(temporaireTableauX[i] + addY), (temporaireTableauY[i] + addX)] == (int)TypeBloc.Gelé)
+          else if (etatBlocs[(temporaireTableauY[i] + addY), (temporaireTableauX[i] + addX)] == (int)TypeBloc.Gelé)
           {
             peutBouger = false;
           }
@@ -305,27 +377,27 @@ namespace TP3
       //Vérification, bouger la pièce du sens horaire
       else if (sens == Deplacement.ROTATION_HORAIRE)
       {
-        int[] temporaireTableauY = new int[4];
         int[] temporaireTableauX = new int[4];
+        int[] temporaireTableauY = new int[4];
         for (int compteur = 0; compteur < blocActifX.Length; compteur++)
         {
-          temporaireTableauY[compteur] = -blocActifY[compteur];
-          temporaireTableauX[compteur] = blocActifX[compteur];
+          temporaireTableauX[compteur] = -blocActifY[compteur];
+          temporaireTableauY[compteur] = blocActifX[compteur];
         }
         for (int i = 0; i < blocActifX.Length; i++)
         {
           // Vérification des côtés du tableau
-          if ((temporaireTableauY[i] + addX) >= nbColonnesJeu || (temporaireTableauY[i] + addX) < 0)
+          if ((temporaireTableauX[i] + addX) >= nbColonnesJeu || (temporaireTableauX[i] + addX) < 0)
           {
             peutBouger = false;
           }
           // Vérification haut et bas du tableau
-          else if ((temporaireTableauX[i] + addY) >= nbLignesJeu || (temporaireTableauX[i] + addY) < 0)
+          else if ((temporaireTableauY[i] + addY) >= nbLignesJeu || (temporaireTableauY[i] + addY) < 0)
           {
             peutBouger = false;
           }
           // Vérification des blocs gelés
-          else if (etatBlocs[(temporaireTableauX[i] + addY), (temporaireTableauY[i] + addX)] == (int)TypeBloc.Gelé)
+          else if (etatBlocs[(temporaireTableauY[i] + addY), (temporaireTableauX[i] + addX)] == (int)TypeBloc.Gelé)
           {
             peutBouger = false;
           }
@@ -335,6 +407,7 @@ namespace TP3
       return peutBouger;
 
     }
+
     /// <summary>
     /// Permet la sortie de l'application à tout moment.
     /// </summary>
@@ -358,6 +431,7 @@ namespace TP3
         }
       }
     }
+
     /// <summary>
     /// Fonction qui entre la position courante des blocs actifs
     /// </summary>
@@ -376,8 +450,9 @@ namespace TP3
         }
       }
     }
+
     /// <summary>
-    /// Timer qui permet la descente des blocs actifs
+    /// Chronomètre qui permet la descente des blocs actifs
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
@@ -390,8 +465,9 @@ namespace TP3
       addY++;
       FaireCouleursBlocs();
     }
+
     /// <summary>
-    /// Fonction qui est appellé lors d'un pèse sur le bouton recommencer partie
+    /// Fonction qui est appellé lorsque l'utilisateur pèse sur le bouton recommencer partie
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
@@ -423,6 +499,7 @@ namespace TP3
       FonctionsJeu();
     }
     //</alangevin>
+
     /// <summary>
     /// Fonction qui assigne la position de départ de la pièce choisie
     /// </summary>
@@ -514,7 +591,7 @@ namespace TP3
     }
 
     /// <summary>
-    /// Fonction qui appelle toute les fonctions nécessaire au fonctionnement du jeu
+    /// Fonction qui appelle toute les fonctions nécessaire à la formation des pièces du jeu
     /// </summary>
     void FonctionsJeu()
     {
@@ -552,30 +629,30 @@ namespace TP3
         //Bouger la pièce du sens horaire
         else if (sens == Deplacement.ROTATION_HORAIRE)
         {
-          int[] temporaireTableauY = new int[4];
+          int[] temporaireTableau = new int[4];
           for (int compteur = 0; compteur < blocActifX.Length; compteur++)
           {
-            temporaireTableauY[compteur] = blocActifX[compteur];
+            temporaireTableau[compteur] = blocActifX[compteur];
           }
           for (int compteur = 0; compteur < blocActifX.Length; compteur++)
           {
             blocActifX[compteur] = -blocActifY[compteur];
-            blocActifY[compteur] = temporaireTableauY[compteur];
+            blocActifY[compteur] = temporaireTableau[compteur];
           }
           FaireCouleursBlocs();
         }
         //Bouger la pièce du sens anti-horaire
         else if (sens == Deplacement.ROTATION_ANTIHORAIRE)
         {
-          int[] temporaireTableauY = new int[4];
+          int[] temporaireTableau = new int[4];
           for (int compteur = 0; compteur < blocActifX.Length; compteur++)
           {
-            temporaireTableauY[compteur] = -blocActifX[compteur];
+            temporaireTableau[compteur] = -blocActifX[compteur];
           }
           for (int compteur = 0; compteur < blocActifX.Length; compteur++)
           {
             blocActifX[compteur] = blocActifY[compteur];
-            blocActifY[compteur] = temporaireTableauY[compteur];
+            blocActifY[compteur] = temporaireTableau[compteur];
           }
           FaireCouleursBlocs();
         }
@@ -592,25 +669,40 @@ namespace TP3
         }
       }
     }
+
+    //<alangevin>
     /// <summary>
     /// Fonction qui permet de vérifier si la partie est terminée
     /// </summary>
-    void VerifierSiPartieTermine()
+    bool VerifierSiPartieTermine()
     {
+      bool partieTermine = false;
       for (int i = 0; i < blocActifY.Length-1; i++)
       {
         if (etatBlocs[(blocActifY[i] + addY), (blocActifX[i] + addX)] == (int)TypeBloc.Gelé)
         {
-          timerDescente.Stop();
-          fin = new FinDePartie();
-          fin.TempsDeJeu(tempsPartie);
-          fin.SpecifierNbPiece(nbrBlocsDifferent);
-          fin.ShowDialog();
-          break;
+          partieTermine = true;
         }
       }
-
+      return partieTermine;
     }
+
+    /// <summary>
+    /// Fonction qui termine la partie
+    /// </summary>
+    void PartieTermine()
+    {
+      if (VerifierSiPartieTermine() == true)
+      {
+        timerDescente.Stop();
+        fin = new FinDePartie();
+        fin.TempsDeJeu(tempsPartie);
+        fin.SpecifierNbPiece(nbrBlocsDifferent);
+        fin.ShowDialog();
+      }
+    }
+    //</alangevin>
+
     //<ADion>
     private void démarrerLaPartieToolStripMenuItem_Click(object sender, EventArgs e)
     {
@@ -639,6 +731,12 @@ namespace TP3
     }
 
     //</ADion>
+
+    /// <summary>
+    /// Fonction qui compte le temps jouer 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void tempsDeJeu_Tick(object sender, EventArgs e)
     {
       tempsPartie++;
